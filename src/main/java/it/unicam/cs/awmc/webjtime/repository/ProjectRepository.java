c67f8408701d328f3cacadc29ab136dab8fec5f8
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-20 19:15:52 +0100

    Extend repository interfaces with custom query methods for Task, Report, and Project entities

--- src/main/java/it/unicam/cs/awmc/webjtime/repository/ProjectRepository.java
+++ src/main/java/it/unicam/cs/awmc/webjtime/repository/ProjectRepository.java
@@ -1,7 +1,11 @@
 package it.unicam.cs.awmc.webjtime.repository;
 
 import it.unicam.cs.awmc.webjtime.model.Project;
+import it.unicam.cs.awmc.webjtime.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByStatus(Status status);
 }
--- src/main/java/it/unicam/cs/awmc/webjtime/repository/ReportRepository.java
+++ src/main/java/it/unicam/cs/awmc/webjtime/repository/ReportRepository.java
@@ -3,5 +3,8 @@ package it.unicam.cs.awmc.webjtime.repository;
 import it.unicam.cs.awmc.webjtime.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    Optional<Report> findByName(String name);
 }
