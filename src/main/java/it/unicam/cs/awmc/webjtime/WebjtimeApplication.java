b523309271043d12eea3474ca6c223281499417a
Redcoral-17 <filippo.corallini@studenti.unicam.it>
2026-02-20 16:39:43 +0100

    Initial commit

---
+++ src/main/java/it/unicam/cs/awmc/webjtime/WebjtimeApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.awmc.webjtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebjtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebjtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unicam/cs/awmc/webjtime/WebjtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.awmc.webjtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebjtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
